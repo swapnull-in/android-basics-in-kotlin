e31699a73871262635dda9bba6c33d4f6d9ab4bf
Swapnil Godambe <swapnil.go20@gmail.com>
2023-09-09 17:33:50 +0530

    adds kotlin behaviour to app

--- unit-01-kotlin-basics/04-add-button-to-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ unit-01-kotlin-basics/04-add-button-to-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
     }
 
     /**
@@ -48,6 +49,38 @@ class MainActivity : AppCompatActivity() {
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+
+        sayLucky()
+    }
+
+    fun sayLucky() {
+
+        val myFirstDice = Dice(6)
+        val rollResult = myFirstDice.roll()
+        val luckyNumber = 4
+
+        when(rollResult) {
+            luckyNumber -> println("You win!")
+            1 -> println("So sorry! You rolled a 1. Try again!")
+            2 -> println("Sadly, you rolled a 2. Try again!")
+            3 -> println("Unfortunately, you rolled a 3. Try again!")
+            5 -> println("Don't cry! You rolled a 5. Try again!")
+            6 -> println("Apologies! You rolled a 6. Try again!")
+        }
+
+//        if(rollResult == luckyNumber) {
+//            println("You win!")
+//        } else if (rollResult == 1) {
+//            println("So sorry! You rolled a 1. Try again!")
+//        } else if (rollResult == 2) {
+//            println("Sadly, you rolled a 2. Try again!")
+//        } else if (rollResult == 3) {
+//            println("Unfortunately, you rolled a 3. Try again!")
+//        } else if (rollResult == 5) {
+//            println("Don't cry! You rolled a 5. Try again!")
+//        } else {
+//            println("Apologies! You rolled a 6. Try again!")
+//        }
     }
 
 }
