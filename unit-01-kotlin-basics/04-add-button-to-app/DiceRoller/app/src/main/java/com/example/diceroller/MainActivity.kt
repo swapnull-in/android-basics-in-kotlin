e9359adbbe370a6e7db6ce7b8a5662db34c9d61f
Swapnil Godambe <swapnil.go20@gmail.com>
2023-09-11 09:37:24 +0530

    add images to the dice roller

--- unit-01-kotlin-basics/04-add-button-to-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ unit-01-kotlin-basics/04-add-button-to-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,7 +17,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -36,51 +36,36 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
 
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        sayLucky()
-    }
-
-    fun sayLucky() {
-
-        val myFirstDice = Dice(6)
-        val rollResult = myFirstDice.roll()
-        val luckyNumber = 4
-
-        when(rollResult) {
-            luckyNumber -> println("You win!")
-            1 -> println("So sorry! You rolled a 1. Try again!")
-            2 -> println("Sadly, you rolled a 2. Try again!")
-            3 -> println("Unfortunately, you rolled a 3. Try again!")
-            5 -> println("Don't cry! You rolled a 5. Try again!")
-            6 -> println("Apologies! You rolled a 6. Try again!")
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
 
-//        if(rollResult == luckyNumber) {
-//            println("You win!")
-//        } else if (rollResult == 1) {
-//            println("So sorry! You rolled a 1. Try again!")
-//        } else if (rollResult == 2) {
-//            println("Sadly, you rolled a 2. Try again!")
-//        } else if (rollResult == 3) {
-//            println("Unfortunately, you rolled a 3. Try again!")
-//        } else if (rollResult == 5) {
-//            println("Don't cry! You rolled a 5. Try again!")
-//        } else {
-//            println("Apologies! You rolled a 6. Try again!")
-//        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 
 }
